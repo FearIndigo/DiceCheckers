5de471d166ff19ff3c11d2194ff8d49789d7c391
FearIndigo <josephm@fearindigo.com.au>
2022-07-16 18:47:32 +0800

    -bugfix -remove ide folder

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.Tilemaps;
